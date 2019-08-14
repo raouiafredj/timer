7af29d44fce42e4288bbcd67a0868615ee2ce134
raouiafredj <raouianour@gmail.com>
2019-08-14 09:55:19 +0200

    first commit

---
+++ my-app/src/Time.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Time.css';
+
+const Time = () =>
+
+{
+   
+
+    return <div className="time-container">
+    <div className="timeContent">
+        <div className="time-digits">
+        00:30:00
+        </div>
+        <div className="time-text">
+            <div className="time-text-item">Hour</div>
+            <div className="time-text-item">Minute</div>
+            <div className="time-text-item">Second</div>
+        </div>
+    </div>
+    </div>   }
+export default Time;
